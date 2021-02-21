95a66ef05a9e92db307520f2b4d0114a675bb3a8
mchau1995 <mchau1995@users.noreply.github.com>
2021-02-22 02:39:00 +0900

    Create Week3.R

---
+++ Week3.R
@@ -0,0 +1,13 @@
+install.packgap_2007 <- filter(gapminder, gapminder$year == 2007)
+ages("tidyverse")
+install.packages("dplyr")
+install.packages("gapminder")
+library(gapminder)
+library(dplyr)
+head(gapminder, 12)
+str(gapminder)
+
+gap_2007 <- filter(gapminder, year == 2007)
+gap_2007
+
+
